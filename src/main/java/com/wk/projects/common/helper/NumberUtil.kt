1b112d0be30a8e3e397e74b41546fbedf0f20425
wk <1226426603@qq.com>
2022-01-05 21:54:05 +0800

    数 工具类 资源获取方法整理 recycleAdapter 接口优化

---
+++ src/main/java/com/wk/projects/common/helper/NumberUtil.kt
@@ -0,0 +1,14 @@
+package com.wk.projects.common.helper
+
+/**
+ *
+ *      author : wk
+ *      e-mail : 1226426603@qq.com
+ *      time   : 2021/12/30
+ *      desc   :
+ *      GitHub : https://github.com/wk1995
+ *      CSDN   : http://blog.csdn.net/qq_33882671
+ * */
+object NumberUtil{
+
+}
