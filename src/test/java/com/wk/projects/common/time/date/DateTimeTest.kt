26420cb23982a51c190981f3aedad74257f57d9b
wk <1226426603@qq.com>
2022-04-16 06:59:17 +0800

    时间相关得测试类

---
+++ src/test/java/com/wk/projects/common/time/date/DateTimeTest.kt
@@ -0,0 +1,21 @@
+package com.wk.projects.common.time.date
+
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * author : wk
+ * e-mail : 1226426603@qq.com
+ * time   : 2022/1/1
+ * desc   :
+ * GitHub : https://github.com/wk1995
+ * CSDN   : http://blog.csdn.net/qq_33882671
+ */
+class DateTimeTest{
+
+    @Test
+    fun test(){
+        print(DateTime.getMonthStart(1640966400000))
+
+    }
+}
